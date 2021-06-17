6ef26020efbaaf8b1a970d0dc431b74ffb35b86a
codedeedope <26957536+codedeedope@users.noreply.github.com>
2021-06-17 12:30:26 +0200

    improved load readability

--- NativeLibLoader/src/main/java/de/dhbw/rahmlab/nativelibloader/api/NativeLibLoader.java
+++ NativeLibLoader/src/main/java/de/dhbw/rahmlab/nativelibloader/api/NativeLibLoader.java
@@ -6,10 +6,10 @@ import de.dhbw.rahmlab.nativelibloader.impl.com.jogamp.common.os.DynamicLibraryB
 import de.dhbw.rahmlab.nativelibloader.impl.com.jogamp.common.os.DynamicLibraryBundleInfo;
 import de.dhbw.rahmlab.nativelibloader.impl.com.jogamp.common.os.Platform;
 import de.dhbw.rahmlab.nativelibloader.impl.com.jogamp.common.util.cache.TempJarCache;
+import de.dhbw.rahmlab.nativelibloader.impl.jogamp.common.Debug;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author fabian
@@ -57,50 +57,26 @@ public class NativeLibLoader {
      * which are wanted to be loaded.
      */
     public void load(List<String> libNames, Class MarkerClass) {
-        List<DynamicLibraryBundleInfo> dynamicLibraryBundleInfos = new ArrayList<>();
-        dynamicLibraryBundleInfos.add(new BundleInfoImpl(libNames));
+        // Prepare parameter
+        DynamicLibraryBundleInfo dynamicLibraryBundleInfo = new BundleInfoImpl(libNames);
+        final Class[] classesFromJavaJars = new Class[]{MarkerClass};
 
         //Init
         Platform.initSingleton();
         TempJarCache.initSingleton();
 
-        /**
-         * Hier werden die .so Dateien aus einem JAR in den JarCache geladen.
-         *
-         * Die Klasse in classesFromJavaJars fungiert dabei als ein Marker. In
-         * dem JAR, wo sich die korrespondierende .class Datei befindet, wird
-         * nach .so Dateien gesucht. Alle dort vorhandenen .so Dateien werden in
-         * den JarCache geladen. Es muss sich dabei nicht um die Klasse handeln,
-         * die mit native Aufrufen von den .so Dateien abhängig ist.
-         *
-         * Wenn die richtigen .so Dateien schon aus einem JAR in
-         * Platform.initSingleton() geladen wurden (wegen gluegen_rt.so), dann
-         * nicht unbedingt notwendig.
-         *
-         * Debug Info: Dort werden die .so Dateien geladen:
-         * //JNILibLoaderbase::addNativeJarLibsImpl(...) [Line 186] (gleiche
-         * Datei) Da werden sie dann in den JarCache geladen: //ok =
-         * TempJarCache.addNativeLibs(classFromJavaJar, nativeJarURI,
-         * nativeLibraryPath); [Line 229]
-         */
-        final Class[] classesFromJavaJars = new Class[]{MarkerClass};
+        // Loads all natives from JAR which contains classesFromJavaJars into TempJarCache.
         JNILibLoaderBase.addNativeJarLibs(classesFromJavaJars, null);
 
-        dynamicLibraryBundles.addAll(
-            dynamicLibraryBundleInfos
-                .stream()
-                .map(bi -> {
-                    //Hier werden die .so Datein (vom JarCache) in die JVM geladen.
-                    return new DynamicLibraryBundle(bi);
-                })
-                .collect(Collectors.toCollection(ArrayList::new)));
+        // Loads LibNames from TempJarCache into the JVM
+        DynamicLibraryBundle dynamicLibraryBundle = new DynamicLibraryBundle(dynamicLibraryBundleInfo);
 
-        dynamicLibraryBundles.forEach(b -> {
-            if (!b.isLibComplete()) {
-                System.out.println("Native lib loading failed: " + b.getBundleInfo().getClass().getCanonicalName());
-            } else {
-                System.out.println("Native lib loading succeeded: " + b.getBundleInfo().getClass().getCanonicalName());
-            }
-        });
+        dynamicLibraryBundles.add(dynamicLibraryBundle);
+
+        if (!dynamicLibraryBundle.isLibComplete()) {
+            System.err.println("Native lib loading failed");
+        } else if (Debug.debugAll()) {
+            System.err.println("Native lib loading succeeded");
+        }
     }
 }
